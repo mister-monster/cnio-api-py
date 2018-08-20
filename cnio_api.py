af19f671219c154a1a5ae01715b9e8d9f24ea5bf
mister-monster <38917788+mister-monster@users.noreply.github.com>
2018-08-20 15:08:30 -0500

    minor changes
    made the code a little less hacky. still have not implemented websockets.

--- cnio_api.py
+++ cnio_api.py
@@ -11,12 +11,12 @@ Read the docs at https://changenow.io/api/docs for more information.
 '''
 
 import requests
-import socketio
-from aiohttp import web
+#import socketio
+#from aiohttp import web
 
 class cnio():
     def __init__(self):
-        self.key = str
+        self.key = str()
         self.api = "https://changenow.io/api/v1/"
 
     def currencies(self, active):
@@ -27,17 +27,17 @@ class cnio():
         endpoint = str(self.api + endpoint + query)
         return requests.get(url=endpoint).content
 
-    def min_amount(self, send, recieve):
+    def min_amount(self, send, receive):
         # this method returns the minimum amount required to make an exchange.
         # you MUST provide the currency pair that you are looking to trade
-        endpoint = str("min-amount/" + send + "_" + recieve)
+        endpoint = str("min-amount/" + send + "_" + receive)
         endpoint = str(self.api + endpoint)
         return requests.get(endpoint).content
 
-    def est_exchange_rate(self, send_amount, send, recieve):
+    def est_exchange_rate(self, send_amount, send, receive):
         # this method returns the amount of <recieve> currency you will get for the provided amount of <send> currency
         # you MUST provide the amount you intend to send, the currency to be sent and the currency to be recieved
-        endpoint = str("exchange-amount/" + str(send_amount) + "/" + send + "_" + recieve)
+        endpoint = str("exchange-amount/" + str(send_amount) + "/" + send + "_" + receive)
         endpoint = str(self.api + endpoint)
         return requests.get(endpoint).content
 
@@ -54,25 +54,25 @@ class cnio():
         self.key = key
 
     # ALL OF THE FOLLOWING ENDPOINTS REQUIRE PROVISION OF AN API KEY
-    def create_transaction(self, send_amount, send, recieve, addr, extra_id=""):
+    def create_transaction(self, send_amount, send, receive, addr, extra_id=""):
         # this endpoint creates a transaction
         # You must provide quite a bit of information. namely, send currency, recieve currency
         # the address you want the recieve currency sent to, the amount you will send,
         # and an "extra ID" for currencies that need it (eg. transaction ID for XMP)
-        query = str("?from=" + send + "&to=" + recieve + "&address=" + addr +
+        query = str("?from=" + send + "&to=" + receive + "&address=" + addr +
                          "&amount=" + str(send_amount) + "&extraId=" + extra_id)
         endpoint = str("transactions/" + self.key)
         endpoint = str(self.api + endpoint + query)
         return requests.post(endpoint).content
 
-    def list_transactions(self, send="", recieve="", status="", limit="", offset=""):
+    def list_transactions(self, send="", receive="", status="", limit="", offset=""):
         # this method returns a list of <limit> transactions that were started using the API key
         # All query parameters are optional, they are: sent currency, recieved currency, transaction status (for a
         # list of available statuses see the changenow API docs), limit (default 10) and offset, which is the
         # number of transactions to skip.
         # if none of these params are provided, the oldes 10 transactions will be returned, regardless
         # of status, currency, etc. No idea why they chose to show the oldest, but whatever.
-        query = str("?from=" + send + "&to=" + recieve + "&status=" + status + "&limit=" + limit + "&offset=" + offset)
+        query = str("?from=" + send + "&to=" + receive + "&status=" + status + "&limit=" + limit + "&offset=" + offset)
         endpoint = str("transactions/" + self.key)
         endpoint = str(self.api + endpoint + query)
         return requests.get(endpoint).content
@@ -84,8 +84,12 @@ class cnio():
         endpoint = str(self.api + endpoint)
         return requests.get(endpoint).content
 
+'''
     def live_tx_updates_socketio(self):
-        # this method interfaces with the sockets.io websockets endpoint and gets live transaction updates
-        z = 1
+        # this method interfaces with the socket.io websockets endpoint and gets live transaction updates
+        # you are probably going to need to call this on a separate thread
         # TODO: implement sockets.io connection and data subscription
+'''
 
+if __name__ != __main__:
+    cnio()
